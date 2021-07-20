f6166bfc96e5fe518cd79a0e80f0a012dd1a7319
Aman Kapoor <aman.kapoor.9015@gmail.com>
2021-07-20 22:47:29 +0530

    responsived 2

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -21,7 +21,7 @@ const OverViewDesigned = (props) => {
                     <div className="innerView" style={{ display: 'flex', flexDirection: 'column', flexWrap: 'nowrap', width: '100%', flex: 1, justifyContent: 'space-between' }}>
                         <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', padding: '0 15px 0 15px' }}>
                             <div style={{ marginTop: '25px' }}>
-                                <p className="name" style={{ textAlign: 'left', fontFamily: 'Poppins', fontWeight: 700, fontSize: '22px', marginTop: '15px' }}>{selectedProp.address}
+                                <p className="name nameoverview" style={{ textAlign: 'left', fontFamily: 'Poppins', fontWeight: 700, fontSize: '22px', marginTop: '15px' }}>{selectedProp.address}
                                 </p>
                             </div>
                             <div style={{ marginTop: '15px' }}>
@@ -37,7 +37,7 @@ const OverViewDesigned = (props) => {
                                 </p>
                             </div>
                             <div style={{ marginRight: '25px' }}>
-                                <span style={{ fontFamily: '"IBM Plex Mono"', fontWeight: 600, fontSize: '18px' }}>{selectedProp.price}</span>
+                                <span class="price" style={{ fontFamily: '"IBM Plex Mono"', fontWeight: 600, fontSize: '18px' }}>{selectedProp.price}</span>
                             </div>
                         </div>
                     </div>
@@ -69,14 +69,14 @@ const Parts = (props) => {
 
     return (
         <div className="view" id="viewId" style={{ display: 'flex', border: '0px transparent', borderImageSlice: 1, borderWidth: '0.5px', borderImageSource: 'linear-gradient(to bottom, white, rgba(0, 0, 0, 0.4))', lineHeight: 1, textAlign: 'center', width: '35%', fontSize: '30px', borderRadius: '23px', backgroundColor: 'white', padding: '1.0px', backgroundImage: 'linear-gradient(white, white), linear-gradient(#fff, #8b8b8b)', backgroundOrigin: 'border-box', backgroundClip: 'content-box, border-box' }}>
-            <img class="img" src={props.src} style={{ alignSelf: 'center', borderRadius: '50%', margin: '12px 0px 12px 10px', height: '100px', width: '100px', boxShadow: '1.08px 1.4px 8.6px -2.1px black' }} />
+            <img class="img" src={props.src} style={{ alignSelf: 'center', borderRadius: '50%', margin: '12px 10px 12px 10px', height: '100px', width: '100px', boxShadow: '1.08px 1.4px 8.6px -2.1px black' }} />
             <div className="innerView" style={{ marginLeft: '5px', marginRight: '5px', display: 'flex', flexDirection: 'column', flexWrap: 'nowrap', width: '100%', flex: 1 }}>
                 <div style={{ marginTop: '5px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', padding: '0 15px 0 15px' }}>
                     <div >
-                        <p className="name" id="viewIdName" style={{ textAlign: 'left', fontFamily: 'Poppins', fontSize: '18px', fontWeight: 500, marginTop: '15px', marginBottom: '0' }}>{props.title.charAt(0).toUpperCase() + props.title.slice(1)}</p>
+                        <p className="name nameid" id="viewIdName" style={{ textAlign: 'left', fontFamily: 'Poppins', fontSize: '18px', fontWeight: 500, marginTop: '15px', marginBottom: '0' }}>{props.title.charAt(0).toUpperCase() + props.title.slice(1)}</p>
                     </div>
                     <div>
-                        <p className="rating" style={{ alignItems: "center",
+                        <p className="rating ratingid" style={{ alignItems: "center",
                         display: "grid",
 gridTemplateColumns:" 1fr 1fr", fontSize: '16px', margin: '10px', backgroundColor: 'black', fontFamily: '"Poppins"', fontWeight: 400, color: 'white', padding: '0 30px 0 30px', borderRadius: '10px', lineHeight: '1.5', textAlign: 'center' }}>{props.stars}<svg style={{ marginLeft: '4px' }} xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                             <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
